3fd684d7f6dc04c29909ba32bd8834889e047ebf
adroxk <adroxk@gmail.com>
2026-02-24 20:26:40 -0600

    Curriculum Vitae

---
+++ src/data/project.js
@@ -0,0 +1,14 @@
+
+export const projects = [
+    {
+        title: "Dulce Amigurumis",
+        description: "Landing optimizada con Astro logrando carga inicial menor a 1s y estructura modular reutilizable.",
+        tech: ["Astro", "Vercel", "CSS"],
+        link: "https://dulce-amigurumis.vercel.app/"
+    },
+    {
+        title: "App Control de Gastos",
+        description: "Aplicación en JavaScript puro para administrar ingresos y egresos.",
+        tech: ["HTML", "CSS", "JavaScript"]
+    }
+];
